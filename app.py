c71c69628b0c6bc1ff92905118eb71e5ce5c5935
K Shivaathmika <shivaathmika2103@gmail.com>
2026-04-09 21:27:39 +0530

    added requiremnets

--- app.py
+++ app.py
@@ -354,8 +354,6 @@ def shop():
     return "Welcome to Shopping 🛍️"
  
  
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
  
